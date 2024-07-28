3ca751e9be81339cea2d9469952a4bc8b89f00c1
Naimul Kabir <kabirnayeem.99@gmail.com>
2024-07-28 15:55:42 +0600

    feat: implement mven central publication

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinxSerialization)
     id("module.publication")
+    alias(libs.plugins.vanniktechMavenPublish)
 }
 
 kotlin {
